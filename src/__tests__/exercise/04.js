07b255630ec6b8e272b8d699456d53d5e7312f96
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-09 14:21:02 +0100

    Testing form submission

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,22 +6,42 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+test('submitting the form calls onSubmit with username and password', async () => {
+
+  // interessante : define a variable to store the submitted data.
+  let submittedData = null;
+
+  // interessante : define an onSubmit prop to pass to the component
+  const handleSubmit = function (formData){
+    submittedData = formData;
+  }
+
+  // interessante : render the component, with the specified prop
+  render(<Login onSubmit={handleSubmit}/>);
+
+  // interessante : get the relevante html elements
+  const userNameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole("button", {name : /submit/i});
+
+  // interessante : Set teste values for the username and password
+  const testUser = "Chuck Norris";
+  const testPass = "chuckypass";
+
+  // interessante : type the values into the fields
+  await userEvent.type(userNameField, testUser);
+  await userEvent.type(passwordField, testPass);
+
+  // interessante : create a "click" userEvent on the submit button
+  await userEvent.click(submitButton);
+
+  const expectedData = {
+    username: testUser,
+    password: testPass
+  };
+
+  // interessante : assert that the values were correctly retrieved from the html elements
+  expect(submittedData).toEqual(expectedData);
 })
 
 /*
