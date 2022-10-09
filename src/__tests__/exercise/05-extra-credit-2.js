9105cdf1682229d0e69df728f27704667aa2b2ed
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-09 17:16:14 +0100

    Extra 5.2

---
+++ src/__tests__/exercise/05-extra-credit-2.js
@@ -0,0 +1,62 @@
+// mocking HTTP requests
+// http://localhost:3000/login-submission
+
+import * as React from 'react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
+import {setupServer} from 'msw/node'
+
+import {handlers} from "../../test/server-handlers";
+
+import Login from '../../components/login-submission'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+
+test(`logging in displays the user's username`, async () => {
+  render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test(`No username means computer says no`, async () => {
+  render(<Login />)
+  const {password} = buildLoginForm()
+
+  // Do not type username
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  
+  expect(screen.getByText(/username required/i)).toBeInTheDocument()
+})
+
+test(`No password means computer says no`, async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  // Do not type password
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  
+  expect(screen.getByText(/password required/i)).toBeInTheDocument()
+})
