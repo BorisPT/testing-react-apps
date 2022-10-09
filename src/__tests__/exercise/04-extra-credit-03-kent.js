d43ede9adabdbff1784707740f28d396127b1a9c
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-09 15:29:38 +0100

    Extra 4.3, kent solution

---
+++ src/__tests__/exercise/04-extra-credit-03-kent.js
@@ -0,0 +1,53 @@
+// form testing
+// http://localhost:3000/login
+
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Login from '../../components/login'
+
+import faker from 'faker'
+
+// interessante : receive an object containing specific properties to override and spread that object
+function buildLoginData(overrides) {
+  return {
+    username : faker.internet.userName(),
+    password : faker.internet.password(),
+    ...overrides
+  }
+}
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  
+  const handleSubmit = jest.fn();
+
+  render(<Login onSubmit={handleSubmit}/>);
+
+  const userNameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole("button", {name : /submit/i});
+
+  // interessante : when calling the method that creates the login data,
+  // override specific properties (like the password, for instance)
+  const {username, password} = buildLoginData({password : "$#enforcedPassword123"});
+
+  // console.log({username, password});
+
+  await userEvent.type(userNameField, username);
+  await userEvent.type(passwordField, password);
+
+  await userEvent.click(submitButton);
+
+  const expectedData = {
+    username,
+    password
+  };
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+  expect(handleSubmit).toHaveBeenCalledWith(expectedData);
+})
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
