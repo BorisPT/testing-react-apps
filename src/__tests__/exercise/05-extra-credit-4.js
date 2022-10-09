e50e027c73aa3a9dc80079c7882e1f9dfb476e4d
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-09 18:20:53 +0100

    Extra 5.4

--- src/__tests__/exercise/05-extra-credit-4.js
+++ src/__tests__/exercise/05-extra-credit-4.js
@@ -11,6 +11,9 @@ import {handlers} from '../../test/server-handlers'
 
 import Login from '../../components/login-submission'
 
+// interessante : we need to re-import the "rest" object so we can define the one-off handlers.
+import {rest} from 'msw'
+
 const buildLoginForm = build({
   fields: {
     username: fake(f => f.internet.userName()),
@@ -22,6 +25,42 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+
+// interessante : after each individual test, reset all the one-off handlers that might have been defined.
+afterEach(() => server.resetHandlers())
+
+
+test(`Server is not responding properly`, async () => {
+
+  // interessante : define a test-specific server handler that will override the
+  // main one, for this test only. We must not forget, however, to reset these
+  // one-off handlers after the test execution
+  server.use(
+    rest.post(
+      // interessante : note that it's the same URL as our app-wide handler
+      // so this will override the other.
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: 'Unknown server error'}))
+      },
+    ),
+  )
+
+  render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  // interessante : define an inline snapshot to compare with the text content of the "alert" div
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"Unknown server error"`,
+  )
+})
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -47,7 +86,7 @@ test(`No username means computer says no`, async () => {
 
   // interessante : inline shapshot
   // In the first execution, with no parameters, Jest will fill in the assertion.
-  // In subsequent executions, if the test fails, Jest will offer to update 
+  // In subsequent executions, if the test fails, Jest will offer to update
   // the assertion
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"username required"`,
@@ -68,3 +107,5 @@ test(`No password means computer says no`, async () => {
     `"password required"`,
   )
 })
+
+
