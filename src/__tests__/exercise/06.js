2605c18623b42a14853667fb7e9f2ce93d238940
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-09 19:33:13 +0100

    Remove comments on my solution

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -13,24 +13,6 @@ beforeAll(() => {
   }
 })
 
-// 💰 I'm going to give you this handy utility function
-// it allows you to create a promise that you can resolve/reject on demand.
-function deferred() {
-  let resolve, reject
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-  return {promise, resolve, reject}
-}
-// 💰 Here's an example of how you use this:
-// const {promise, resolve, reject} = deferred()
-// promise.then(() => {/* do something */})
-// // do other setup stuff and assert on the pending state
-// resolve()
-// await promise
-// // assert on the resolved state
-
 test('displays the users current location', async () => {
 
   // interessante : define a fake position to be returned by the mocked function
@@ -42,7 +24,8 @@ test('displays the users current location', async () => {
   }
 
   // interessante : define the mock implementation of the "getCurrentPosition" as function.
-  // This function accepts a callback as an argument and invokes it with the fakePosition data
+  // This function accepts a callback as an argument and invokes it with the fakePosition data,
+  // when the timeout expires. This will give us time to simulate the loading screen
   window.navigator.geolocation.getCurrentPosition.mockImplementation(
     function (callback) {
       setTimeout(() => {
@@ -60,44 +43,6 @@ test('displays the users current location', async () => {
   expect(screen.getByText(/latitude/i)).toHaveTextContent(/35/i);
   expect(screen.getByText(/longitude/i)).toHaveTextContent(/129/i);
 
-
-  // 🐨 create a fakePosition object that has an object called "coords" with latitude and longitude
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
-  //
-  // 🐨 create a deferred promise here
-  //
-  // 🐨 Now we need to mock the geolocation's getCurrentPosition function
-  // To mock something you need to know its API and simulate that in your mock:
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-  //
-  // here's an example of the API:
-  // function success(position) {}
-  // function error(error) {}
-  // navigator.geolocation.getCurrentPosition(success, error)
-  //
-  // 🐨 so call mockImplementation on getCurrentPosition
-  // 🐨 the first argument of your mock should accept a callback
-  // 🐨 you'll call the callback when the deferred promise resolves
-  // 💰 promise.then(() => {/* call the callback with the fake position */})
-  //
-  // 🐨 now that setup is done, render the Location component itself
-  //
-  // 🐨 verify the loading spinner is showing up
-  // 💰 tip: try running screen.debug() to know what the DOM looks like at this point.
-  //
-  // 🐨 resolve the deferred promise
-  // 🐨 wait for the promise to resolve
-  // 💰 right around here, you'll probably notice you get an error log in the
-  // test output. You can ignore that for now and just add this next line:
-  // act(() => {})
-  //
-  // If you'd like, learn about what this means and see if you can figure out
-  // how to make the warning go away (tip, you'll need to use async act)
-  // 📜 https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
-  //
-  // 🐨 verify the loading spinner is no longer in the document
-  //    (💰 use queryByLabelText instead of getByLabelText)
-  // 🐨 verify the latitude and longitude appear correctly
 })
 
 /*
