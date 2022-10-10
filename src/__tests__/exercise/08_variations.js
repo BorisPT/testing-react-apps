1f8b27600fde645bb3e0032a3c6be6a2dae56aea
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-10 13:56:05 +0100

    Ex 8 with variations

---
+++ src/__tests__/exercise/08_variations.js
@@ -0,0 +1,65 @@
+// testing custom hooks
+// http://localhost:3000/counter-hook
+
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import useCounter from '../../components/use-counter'
+
+// interessante : define a simple component that uses the useCounter hook
+const SimpleComponent = ({initialCount = 0}) => { 
+
+  const {count, increment, decrement} = useCounter({initialCount});
+
+  return (
+    // interessante : expose some UI elements and buttons to change them
+    <div>      
+      <div >Counter: {count}</div>
+      <button onClick={increment}>Inc</button>
+      <button onClick={decrement}>Dec</button>
+    </div>
+  )
+ };
+
+test('exposes the count and increment/decrement functions', async () => {
+
+  // interessante : render the component
+  render(<SimpleComponent />)
+
+  // interessante : select the elements for interaction
+  const counterDiv = screen.getByText(/counter/i)
+  const incButton = screen.getByRole("button", {name : /inc/i})
+  const decButton = screen.getByRole("button", {name : /dec/i})
+
+  // initial assertion
+  expect(counterDiv).toHaveTextContent(/0/i)
+
+  await userEvent.click(incButton)
+  expect(counterDiv).toHaveTextContent(/1/i)
+
+  await userEvent.click(decButton) 
+  expect(counterDiv).toHaveTextContent(/0/i)
+})
+
+test('Test a different initial count', async () => {
+
+  render(<SimpleComponent initialCount={3}/>)
+
+  // interessante : select the elements for interaction
+  const counterDiv = screen.getByText(/counter/i)
+  const incButton = screen.getByRole("button", {name : /inc/i})
+  const decButton = screen.getByRole("button", {name : /dec/i})
+
+  screen.debug()
+
+  // initial assertion
+  expect(counterDiv).toHaveTextContent(/3/i)
+
+  await userEvent.click(incButton)
+  expect(counterDiv).toHaveTextContent(/4/i)
+
+  await userEvent.click(decButton) 
+  expect(counterDiv).toHaveTextContent(/3/i)
+})
+
+/* eslint no-unused-vars:0 */
