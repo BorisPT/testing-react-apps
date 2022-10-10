052f2cf672ddb876b9019d0413b663253b6733d0
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-10 16:51:18 +0100

    Extra 8.3

--- src/__tests__/exercise/08-extra-credit-3.js
+++ src/__tests__/exercise/08-extra-credit-3.js
@@ -4,60 +4,64 @@
 import * as React from 'react'
 
 // interessante : Import the "renderHook" function which allows us to 
-// invoke a custom hook much like our own "setup" function did
+// invoke a custom hook, much like our own "setup" function did
 import {renderHook, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
 test('Fake component, normal initial count and step', async () => {
 
-  const hookResult = renderHook();
+  // interessante : invoke the custom hook and destructure
+  // the "result" field. 
+  const {result} = renderHook(useCounter);
+  
+  // interessante : access the "current" property where the output
+  // of the custom hook will be stored
+  expect(result.current.count).toBe(0);
 
-  expect(hookResult.count).toBe(0);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(1);
 
-  act(() => hookResult.increment());
-  expect(hookResult.count).toBe(1);
-
-  act(() => hookResult.decrement());
-  expect(hookResult.count).toBe(0);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
 })
 
 test('Fake component, configure initial count', async () => {
   
-  const hookResult = renderHook({initialProps : {initialCount : 5}})
+  const {result} = renderHook(useCounter, {initialProps : {initialCount : 5}})
 
-  expect(hookResult.count).toBe(5);
+  expect(result.current.count).toBe(5);
 
-  act(() => hookResult.increment());
-  expect(hookResult.count).toBe(6);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(6);
 
-  act(() => hookResult.decrement());
-  expect(hookResult.count).toBe(5);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(5);
 })
 
 test('Fake component, configure step', async () => {
   
-  const hookResult = renderHook({initialProps : {step : 9}})
+  const {result} = renderHook(useCounter, {initialProps : {step : 9}})
 
-  expect(hookResult.count).toBe(0);
+  expect(result.current.count).toBe(0);
 
-  act(() => hookResult.increment());
-  expect(hookResult.count).toBe(9);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(9);
 
-  act(() => hookResult.decrement());
-  expect(hookResult.count).toBe(0);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
 })
 
 test('Fake component, configure initial count and step', async () => {
   
-  const hookResult = renderHook({initialProps : {initialCount : 2, step : 9}})
+  const {result} = renderHook(useCounter, {initialProps : {initialCount : 2, step : 9}})
 
-  expect(hookResult.count).toBe(2);
+  expect(result.current.count).toBe(2);
 
-  act(() => hookResult.increment());
-  expect(hookResult.count).toBe(11);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(11);
 
-  act(() => hookResult.decrement());
-  expect(hookResult.count).toBe(2);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(2);
 })
 
 /* eslint no-unused-vars:0 */
