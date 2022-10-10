fccaf4ae4374ccc9a7011781606d9947c3c93c66
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-10-10 09:19:40 +0100

    My version, exercise 7

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -5,18 +5,41 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
+import userEvent from '@testing-library/user-event'
 
-test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+// interessante : define a wrapper function that will encapsulate 
+// the component we want to test with all the contexts it needs
+function ContextWrapper({children}) {
+  return <ThemeProvider>{children}</ThemeProvider>
+}
+
+test('renders with the light styles for the light theme', async () => {
+
+  // interessante : provide a wrapper function to be applied to the component.
+  // This will inject all the contexts that the component needs to render.
+  // Also, we destructure a "rerender" function to be called after pressing the button
+  const {rerender} = render(<EasyButton>Easy</EasyButton>, {wrapper: ContextWrapper})
+
+  const button = screen.getByRole('button', {name: /easy/i})
+
+  // interessante : initial assertion about the button style
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+
+  // interessante : click the button
+  await userEvent.click(button);
+
+  // interessante : trigger a re-render
+  rerender(<EasyButton>Easy</EasyButton>)
+
+  // interessante : assert that the style has been updated
+  expect(button).toHaveStyle(`
+  backgroundColor: 'black',
+  color: 'white',
+`)
+
 })
 
 /* eslint no-unused-vars:0 */
